8511d0f0d6cd13f157a05da443b0f03361bed2bd
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2025-10-08 19:45:47 +0200

    UPPGIFT ETT WINDOWS FORMS

---
+++ L0002B_Uppgift1__WindowsForms_Bjarne_Rossen/L0002B_Uppgift1__WindowsForms_Bjarne_Rossen/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace L0002B_Uppgift1__WindowsForms_Bjarne_Rossen
+{
+    partial class Vaxelberakning
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPris = new System.Windows.Forms.Label();
+            this.txtPris = new System.Windows.Forms.TextBox();
+            this.lblBetalt = new System.Windows.Forms.Label();
+            this.txtBetalt = new System.Windows.Forms.TextBox();
+            this.btnBerakna = new System.Windows.Forms.Button();
+            this.txtResultat = new System.Windows.Forms.TextBox();
+            this.btnAvsluta = new System.Windows.Forms.Button();
+            this.grpboxResultat = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // lblPris
+            // 
+            this.lblPris.AutoSize = true;
+            this.lblPris.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPris.Location = new System.Drawing.Point(53, 54);
+            this.lblPris.Name = "lblPris";
+            this.lblPris.Size = new System.Drawing.Size(78, 13);
+            this.lblPris.TabIndex = 0;
+            this.lblPris.Text = "Ange pris (kr):";
+            // 
+            // txtPris
+            // 
+            this.txtPris.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPris.Location = new System.Drawing.Point(131, 51);
+            this.txtPris.Name = "txtPris";
+            this.txtPris.Size = new System.Drawing.Size(136, 21);
+            this.txtPris.TabIndex = 1;
+            // 
+            // lblBetalt
+            // 
+            this.lblBetalt.AutoSize = true;
+            this.lblBetalt.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBetalt.Location = new System.Drawing.Point(53, 83);
+            this.lblBetalt.Name = "lblBetalt";
+            this.lblBetalt.Size = new System.Drawing.Size(64, 13);
+            this.lblBetalt.TabIndex = 2;
+            this.lblBetalt.Text = "Betalt (kr) :";
+            // 
+            // txtBetalt
+            // 
+            this.txtBetalt.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBetalt.Location = new System.Drawing.Point(131, 80);
+            this.txtBetalt.Name = "txtBetalt";
+            this.txtBetalt.Size = new System.Drawing.Size(136, 21);
+            this.txtBetalt.TabIndex = 3;
+            // 
+            // btnBerakna
+            // 
+            this.btnBerakna.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBerakna.Location = new System.Drawing.Point(56, 118);
+            this.btnBerakna.Name = "btnBerakna";
+            this.btnBerakna.Size = new System.Drawing.Size(153, 36);
+            this.btnBerakna.TabIndex = 4;
+            this.btnBerakna.Text = "Beräkna växel";
+            this.btnBerakna.UseVisualStyleBackColor = true;
+            this.btnBerakna.Click += new System.EventHandler(this.btnBerakna_Click);
+            // 
+            // txtResultat
+            // 
+            this.txtResultat.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultat.Location = new System.Drawing.Point(56, 191);
+            this.txtResultat.Multiline = true;
+            this.txtResultat.Name = "txtResultat";
+            this.txtResultat.ReadOnly = true;
+            this.txtResultat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultat.Size = new System.Drawing.Size(211, 186);
+            this.txtResultat.TabIndex = 5;
+            // 
+            // btnAvsluta
+            // 
+            this.btnAvsluta.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvsluta.Location = new System.Drawing.Point(215, 118);
+            this.btnAvsluta.Name = "btnAvsluta";
+            this.btnAvsluta.Size = new System.Drawing.Size(52, 36);
+            this.btnAvsluta.TabIndex = 6;
+            this.btnAvsluta.Text = "Avsluta";
+            this.btnAvsluta.UseVisualStyleBackColor = true;
+            this.btnAvsluta.Click += new System.EventHandler(this.btnAvsluta_Click);
+            // 
+            // grpboxResultat
+            // 
+            this.grpboxResultat.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpboxResultat.Location = new System.Drawing.Point(49, 172);
+            this.grpboxResultat.Name = "grpboxResultat";
+            this.grpboxResultat.Size = new System.Drawing.Size(229, 220);
+            this.grpboxResultat.TabIndex = 7;
+            this.grpboxResultat.TabStop = false;
+            this.grpboxResultat.Text = "Växel tillbaka:";
+            // 
+            // Vaxelberakning
+            // 
+            this.AcceptButton = this.btnBerakna;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 426);
+            this.Controls.Add(this.txtResultat);
+            this.Controls.Add(this.grpboxResultat);
+            this.Controls.Add(this.btnAvsluta);
+            this.Controls.Add(this.btnBerakna);
+            this.Controls.Add(this.txtBetalt);
+            this.Controls.Add(this.lblBetalt);
+            this.Controls.Add(this.txtPris);
+            this.Controls.Add(this.lblPris);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Vaxelberakning";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Växelberäkning";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPris;
+        private System.Windows.Forms.TextBox txtPris;
+        private System.Windows.Forms.Label lblBetalt;
+        private System.Windows.Forms.TextBox txtBetalt;
+        private System.Windows.Forms.Button btnBerakna;
+        private System.Windows.Forms.TextBox txtResultat;
+        private System.Windows.Forms.Button btnAvsluta;
+        private System.Windows.Forms.GroupBox grpboxResultat;
+    }
+}
+
---
+++ L0002B_Uppgift1__WindowsForms_Bjarne_Rossen/L0002B_Uppgift1__WindowsForms_Bjarne_Rossen/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L0002B_Uppgift1__WindowsForms_Bjarne_Rossen
+{
+    public partial class Vaxelberakning : Form
+    {
+        // Konstanta valörer (singular, plural)
+        readonly (int varde, string singular, string plural)[] valorer =
+        {
+            (500, "femhundralapp", "femhundralappar"),
+            (200, "tvåhundralapp", "tvåhundralappar"),
+            (100, "hundralapp", "hundralappar"),
+            (50,  "femtiolapp", "femtiolappar"),
+            (20,  "tjugolapp", "tjugolappar"),
+            (10,  "tiokrona", "tiokronor"),
+            (5,   "femkrona", "femkronor"),
+            (1,   "krona", "kronor")
+        };
+        public Vaxelberakning()
+        {
+            InitializeComponent();
+            // Gör Enter/Return till beräkna-knapp
+            this.AcceptButton = btnBerakna;
+            // Säkerställ att resultatrutan är readonly och multiline (kan också ställas i Designer)
+            txtResultat.ReadOnly = true;
+            txtResultat.Multiline = true;
+            txtResultat.ScrollBars = ScrollBars.Vertical;
+        }
+
+        // När användaren klickar på "Beräkna växel"
+        private void btnBerakna_Click(object sender, EventArgs e)
+        {
+            // Validera pris
+            if (!int.TryParse(txtPris.Text.Trim(), out int pris) || pris <= 0)
+            {
+                MessageBox.Show("Ange ett giltigt pris (positivt heltal).", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPris.Focus();
+                return;
+            }
+
+            // Validera betalt - vi summerar inte tidigare värden, användaren matar in nytt varje gång
+            if (!int.TryParse(txtBetalt.Text.Trim(), out int betalt) || betalt < 0)
+            {
+                MessageBox.Show("Ange ett giltigt betalt belopp (positivt heltal).", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtBetalt.Focus();
+                return;
+            }
+
+            // Om betalt är för lite: visa meddelande, markera betalt-fältet och avbryt (pris bibehålls)
+            if (betalt < pris)
+            {
+                MessageBox.Show($"Kunden har betalat för lite! {pris - betalt} kr saknas.", "För lite", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtBetalt.SelectAll();
+                txtBetalt.Focus();
+                return;
+            }
+
+            // Räkna växel och skriv ut kvitto + valörer i txtResultat
+            int vaxel = betalt - pris;
+            txtResultat.Clear();
+            txtResultat.AppendText("=== KVITTO ===\r\n");
+            txtResultat.AppendText($"Pris:   {pris} kr\r\n");
+            txtResultat.AppendText($"Betalt: {betalt} kr\r\n");
+            txtResultat.AppendText($"Växel:  {vaxel} kr\r\n\r\n");
+
+            if (vaxel == 0)
+            {
+                txtResultat.AppendText("Ingen växel behövs. Tack för köpet!\r\n");
+            }
+            else
+            {
+                txtResultat.AppendText("Växel tillbaka:\r\n");
+                foreach (var v in valorer)
+                {
+                    int antal = vaxel / v.varde;
+                    if (antal > 0)
+                    {
+                        string namn = antal == 1 ? v.singular : v.plural;
+                        txtResultat.AppendText($"{antal} {namn}\r\n");
+                        vaxel %= v.varde;
+                    }
+                }
+                txtResultat.AppendText("\r\nTack för köpet!\r\n");
+            }
+        }
+
+        // Avsluta-knappen
+        private void btnAvsluta_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+ }
